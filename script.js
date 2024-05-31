7e4001fe778ea6a05274fa9043b62f0470548e3b
anisenson <adamjn524@gmail.com>
2024-05-31 09:18:23 -0400

    Added Review slideshow

---
+++ script.js
@@ -0,0 +1,33 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function changeSlide(n) {
+  showSlides(slideIndex += n);
+}
+
+function setCurrentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  const slides = document.getElementsByClassName("mySlides");
+  const dots = document.getElementsByClassName("dot");
+
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+
+  for (let slide of slides) {
+    slide.style.display = "none";
+  }
+
+  for (let dot of dots) {
+    dot.className = dot.className.replace(" active", "");
+  }
+
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
