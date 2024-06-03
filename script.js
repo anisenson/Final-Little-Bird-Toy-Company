27480f30961f5846c5da3d3ad5ef6402189eb40c
Adam Nisenson <144464774+anisenson@users.noreply.github.com>
2024-06-03 18:43:53 +0000

    Added actual content

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const Products = [
     {
-        product: 'Cars',
+        product: 'Wooden Cars',
         price: '$2.99',
-        description: 'these cars have a wonderful description',
-        age: '2-3 years old',
-        measures: '4x by 10x',
+        description: 'This heirloom-quality wooden car is a timeless treasure. Handcrafted from domestic and exotic hardwoods with a clear lacquer finish, this unique car will inspire generations of imaginative play. Please note potential choking hazards for small children.',
+        age: 'All Ages',
+        measures: 'N/A',
         cardimg: 'images/car1.jpg',
         img1: 'images/car1.jpg',
         img2: 'images/car2.jpg',
@@ -13,31 +13,57 @@ const Products = [
         modalID: 'modal2'
     },
     {
-        product: 'Trains',
-        price: '$3.99',
-        description: 'these trains are fantastic',
-        age: '4-5 years old',
-        measures: '5x by 12x',
+        product: 'Wooden Train Set',
+        price: '$10.99',
+        description: 'Embark on a charming journey with this beautiful handcrafted wooden train set. Engine and three interchangeable cars boast intricate details made from real beech wood. Large size with moving wheels and a fully ecological design.',
+        age: '3+',
+        measures: '(84cm L x 11cm H x 13cm W)',
         cardimg: 'images/train1.jpg',
         img1: 'images/train1.jpg',
         img2: 'images/train2.jpg',
         img3: 'images/train3.jpg',
-        img4: 'images/train4.jpg',
+        img4: 'images/train7.jpg',
         modalID: 'modal3'
     },
     {
-        product: 'Planes',
+        product: 'Wooden Airplane',
         price: '$4.99',
-        description: 'these planes are amazing',
-        age: '6-7 years old',
-        measures: '6x by 14x',
+        description: 'Soar through imaginative skies with this classic wooden airplane. Handcrafted from sustainable Baltic birch wood with a safe, natural harvest finish and a spinning propeller. ',
+        age: 'Toddlers',
+        measures: '(3.5"H x 7"L x 7"W.)',
         cardimg: 'images/plane1.jpg',
         img1: 'images/plane1.jpg',
         img2: 'images/plane2.jpg',
         img3: 'images/plane3.jpg',
         img4: 'images/plane4.jpg',
         modalID: 'modal4'
-    }
+    },
+    {
+        product: 'Wooden Boat',
+        price: '$3.99',
+        description: 'Set sail for bathtub adventures with this adorable wooden boat. Made from solid Maine white pine, this handcrafted toy floats and features rounded edges for safety.',
+        age: 'All Ages',
+        measures: '(10.5"W x 3.5"H)',
+        cardimg: 'images/boat1.jpg',
+        img1: 'images/boat2.jpg',
+        img2: 'images/boat3.jpg',
+        img3: 'images/boat4.jpg',
+        img4: 'images/boat5.jpg',
+        modalID: 'modal5'
+    },
+    {
+        product: 'Wooden Block Set',
+        price: '$12.99',
+        description: 'Build creativity and imagination with this high-quality, 72-piece block set. Made from naturally finished and smooth-sanded hardwood blocks, this set comes in a convenient wooden storage crate',
+        age: '3+',
+        measures: '(13” L x 12” W x 2” H).',
+        cardimg: 'images/block1.jpg',
+        img1: 'images/block2.jpg',
+        img2: 'images/block3.jpg',
+        img3: 'images/block4.jpg',
+        img4: 'images/block5.jpg',
+        modalID: 'modal6'
+    },
 ];
 
 function generateProductCards() {
@@ -71,9 +97,9 @@ function generateProductCards() {
                                     <img class="smallpic" id="smallpic${index}-3" src="${product.img4}">
                                 </div>    
                                 <div class="col-12">
-                                    <h3>${product.description}</h3>
-                                    <h5>${product.age}</h5>
-                                    <h6>${product.measures}</h6>
+                                    <h4>Description:<br>${product.description}</h4>
+                                    <h5>Age: ${product.age}</h5>
+                                    <h6>Measures: ${product.measures}</h6>
                                 </div>
                             </div>
                         </div>
