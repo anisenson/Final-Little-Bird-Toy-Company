21da70220a6a35e2a188cf51f69e1e03410fc774
anisenson <adamjn524@gmail.com>
2024-06-03 10:36:44 -0400

    "added some inspirational sh**"

--- script.js
+++ script.js
@@ -1,42 +1,23 @@
-let slideIndex = 1;
-showSlides(slideIndex);
-
-function changeSlide(n) {
-  showSlides(slideIndex += n);
-}
-
-function setCurrentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  const slides = document.getElementsByClassName("mySlides");
-  const dots = document.getElementsByClassName("dot");
-
-  if (n > slides.length) {
-    slideIndex = 1;
-  }
-  if (n < 1) {
-    slideIndex = slides.length;
-  }
-
-  for (let slide of slides) {
-    slide.style.display = "none";
-  }
-
-  for (let dot of dots) {
-    dot.className = dot.className.replace(" active", "");
-  }
-
-  slides[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].className += " active";
-}
 
 
 
-// learn more button //
 
+// Modal picture js //
 
+document.addEventListener('DOMContentLoaded', function() {
+  const mainImage = document.getElementById('mainPic');
+  const smallImages = document.querySelectorAll('.smallpic');
+
+  smallImages.forEach(smallImage => {
+      smallImage.addEventListener('mouseenter', function() {
+          mainImage.src = smallImage.src;
+      });
+
+      smallImage.addEventListener('mouseleave', function() {
+          mainImage.src = 'images/car1.jpg'; 
+      });
+  });
+});
 
 
 
