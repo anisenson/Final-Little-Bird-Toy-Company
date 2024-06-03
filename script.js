20831a2ad1ec6e816159f92c949bdcd16b3e71de
anisenson <adamjn524@gmail.com>
2024-06-03 10:43:12 -0400

    added comments and formated

--- script.js
+++ script.js
@@ -6,9 +6,9 @@
 
 document.addEventListener('DOMContentLoaded', function() {
   const mainImage = document.getElementById('mainPic');
-  const smallImages = document.querySelectorAll('.smallpic');
+  const smallImages = document.querySelectorAll('.smallpic'); //takes all id's with "smallpic*" and asigns it to smallImage
 
-  smallImages.forEach(smallImage => {
+  smallImages.forEach(smallImage => { //iterates through all smallimages to make the changes on hover.
       smallImage.addEventListener('mouseenter', function() {
           mainImage.src = smallImage.src;
       });
